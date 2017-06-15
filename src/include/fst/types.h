34046332296b19a7dcc487cdb3a1d508ebd2375f
kkm <kkm@smartaction.com>
2017-06-15 13:50:42 -0700

    VS project and solution for static libfst only

--- src/include/fst/types.h
+++ src/include/fst/types.h
@@ -15,6 +15,7 @@
 //
 // Various type definitions (mostly for Google compatibility).
 
+#include <cstddef>       // For std::ptrdiff_t.
 #include <cstdlib>       // for ssize_t.
 #include <cstdint>       // for ?int*_t.
 
@@ -30,5 +31,9 @@ using uint8 = uint8_t;
 using uint16 = uint16_t;
 using uint32 = uint32_t;
 using uint64 = uint64_t;
+
+// Not really Windows-specific: they should have used ptrdiff_t in the first
+// place. But on Windows there has never been ssize_t.
+using ssize_t = std::ptrdiff_t;
 
 #endif  // FST_LIB_TYPES_H_
